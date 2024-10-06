73a8224e542ee7da98f30a2ada605f3497cc3fee
憲也 高木 <live201x@gmail.com>
2024-10-06 22:28:17 +0900

    s6-42

--- src/main/java/com/example/its/web/user/UserForm.java
+++ src/main/java/com/example/its/web/user/UserForm.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Data
 @AllArgsConstructor
@@ -16,6 +17,7 @@ public class UserForm {
     private String username;
 
     @NotBlank
+    @Size(min = 12, max = 128)
     private String password;
 
 
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,5 +2,7 @@ insert into issues (summary, description) values ('バグA', 'バグがありま
 insert into issues (summary, description) values ('機能要望B', 'Bに追加機能がほしいです');
 insert into issues (summary, description) values ('画面Cが遅い', '早くしてほしいです');
 
-insert into users (username, password) values ('tom', 'password');
-insert into users (username, password) values ('bob', 'password');
+
+--password1234
+insert into users (username, password) values ('tom', 'bbe15318b8f84b8af7f254fafff48525def2d4d84d8d8c1c9f32018549df39c8cc5cf6b4a78d838a');
+insert into users (username, password) values ('bob', '4b1618f92c9c23fb9b7c3ff552b72e1f7a97dbfd014d721fa6ee9aa8eb803bea113f2d09af8ec74f');
